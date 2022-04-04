e2b4cff3aa9cca23edb0430c93817ee136f5a2aa
landroide13 <landrade1313@gmail.com>
2022-04-05 11:15:21 +1200

    getting data

--- binSer/Program.cs
+++ binSer/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
 
 namespace binSer
 {
     class Program
     {
+        static List<string> records = new List<string>();
+        static void getData()
+        {
+            Console.WriteLine("######## Please enter ID ########");
+
+            string id = Console.ReadLine();
+
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader("/Users/landrade/Desktop/Whitecleffe/DataStructure/Assign1/Question1/listOfPatients.txt");
+            
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    records.Add(line);
+                 
+                    line = sr.ReadLine();
+                }
+
+                 foreach(string el in records)
+                {
+                        Console.WriteLine("Arr " + el);
+                }
+
+                
+                
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+
+
     }
 }
