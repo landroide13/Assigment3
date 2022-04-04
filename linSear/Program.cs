fb4389c68618e1a3da2af57532d7bed380f7809c
landroide13 <landrade1313@gmail.com>
2022-04-05 10:57:49 +1200

    start

---
+++ linSear/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linSear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
